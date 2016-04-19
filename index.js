1a86fa9a41007eb5a67ca170ac9b86ca9092d603
Ed Watts <ed.watts@gmail.com>
2016-04-19 20:34:46 +0100

    re-organised index.js code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 var iconv = require('iconv-lite');
 var Converter = require("csvtojson").Converter;
+//create a new csvtojson converter
 var converter = new Converter({
   delimiter: '|',
   noheader: true
 });
-
+//set decodestream encoding(decoding)
+var converterStream = iconv.decodeStream('utf16');
+//set JSON scheme
 var jsonOutput = {
   "license-url": "http://creativecommons.org/licenses/by/4.0/",
   "readme": "Default content to be built into distributable apk. Translations by: http://translatorswithoutborders.org and http://mercycorps.org",
@@ -27,17 +30,11 @@ var jsonOutput = {
     ]
   }]
 };
-var converterStream = iconv.decodeStream('utf16');
-
-converter.on("end_parsed", function() {
-  var json2write = JSON.stringify(jsonOutput, null, 4);
-  fs.writeFile('data.json', json2write, function (err) {
-    if (err) return console.log(err);
-    console.log('Data written to JSON!');
-  });
-
-});
-
+//get file name from cli args
+var filename = process.argv[2];
+//read file & pipe through decoder & send to csvtojson converter
+var file = fs.createReadStream(filename).pipe(converterStream).pipe(converter);
+//handle each record appropriately
 converter.on("record_parsed", function(jsonObj) {
   if (jsonObj.field1 === "META:Background");
   else {
@@ -53,8 +50,11 @@ converter.on("record_parsed", function(jsonObj) {
     }
   }
 });
-
-var filename = process.argv[2];
-
-var file = fs.createReadStream(filename);
-file.pipe(converterStream).pipe(converter);
+//When finished converting stringify json and write to file
+converter.on("end_parsed", function() {
+  var json2write = JSON.stringify(jsonOutput, null, 4);
+  fs.writeFile('data.json', json2write, function (err) {
+    if (err) return console.log(err);
+    console.log('Data written to JSON!');
+  });
+});
